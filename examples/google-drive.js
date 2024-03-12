2e17ec34a4dbdba5946abea834e00e5e466031fd
Dominik Tarnowski <dom@tdom.dev>
2024-03-12 20:41:52 +0000

    update indentation

--- examples/google-drive.js
+++ examples/google-drive.js
@@ -7,50 +7,50 @@ z.init(store)
 // some utils, the format is hard coded
 
 function is(l) {
-	return `is:${l}`
+    return `is:${l}`
 }
 
 function set(namespace, id) {
-	const s = `${namespace}:${id}`
-	return s
+    const s = `${namespace}:${id}`
+    return s
 }
 
 function checkAny(namespaces, id, label) {
-	for (let n of namespaces) {
-		const ok = z.check(set(n, id), label) 
-		if (ok) return true
-	}
-  return false
+    for (let n of namespaces) {
+	const ok = z.check(set(n, id), label)
+	if (ok) return true
+    }
+    return false
 }
 
 // implementation
 
 
 function onCreateFile(folderId, fileId, ownerId) {
-	const relations = ["owner", "viewer", "commenter", "editor"]
-	for (let relation of relations) {
-		z.addSubset(set(`file_${relation}s`, fileId), set(`folder_${relation}s`, folderId))
-	}
-	z.label(set("file_owners", fileId), ownerId)
+    const relations = ["owner", "viewer", "commenter", "editor"]
+    for (let relation of relations) {
+	z.addSubset(set(`file_${relation}s`, fileId), set(`folder_${relation}s`, folderId))
+    }
+    z.label(set("file_owners", fileId), ownerId)
 }
 
 function shareFolderWithDomain(folderId, domainId) {
-	z.addSubset(set("folder_owners", folderId), set("domain_members", domainId))
+    z.addSubset(set("folder_owners", folderId), set("domain_members", domainId))
 }
 
 function addUserToDomain(domainId, userId) {
-	z.label(set("domain_members", domainId), userId)
+    z.label(set("domain_members", domainId), userId)
 }
 
 function canViewFile(fileId, userId) {
-	const isPublic = z.check(set("file_labels", fileId), "public")
-	if (isPublic) return true
+    const isPublic = z.check(set("file_labels", fileId), "public")
+    if (isPublic) return true
 
-	return checkAny(
-    ["file_owners", "file_comenters", "file_editors", "file_viewers"],
-		fileId,
-		userId,
-	)
+    return checkAny(
+	["file_owners", "file_comenters", "file_editors", "file_viewers"],
+	fileId,
+	userId,
+    )
 }
 
 // usage examples
@@ -65,4 +65,3 @@ console.log(canViewFile("salaries.pdf", "bob"))
 
 z.label(set("file_labels", "salaries.pdf"), "public")
 console.log(canViewFile("salaries.pdf", "-"))
-
